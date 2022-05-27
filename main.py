948804a28c7bb486cfee53ea2226627d5bdc48c1
Daniel <60565717+Galiafer@users.noreply.github.com>
2022-05-26 20:21:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,18 +18,15 @@ def getTranslation():
 config = getConfig()
 # get translations
 translation = getTranslation()
-# if windows True, else False (mac, linux)
+# if Windows True, else False (mac, linux)
 isWindows = True if os.name == 'nt' else False
 # if language = 'ru' set translation to Russian, else English
 language = 'ru' if locale.getdefaultlocale()[0] == "ru_RU" else 'en'
 
-# if mint on magiceden.io
 if "magiceden.io" in config['launchpadLink']:
     print(translation[language]['found'])
     bot = MintBot(config, translation, language, isWindows)
 
     bot.start()
-
-# if platform not supported
 else:
     print("Could not recognize link")
