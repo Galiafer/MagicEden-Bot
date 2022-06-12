1817bb06fbf1b3618a1ff3ec22654f3414303cfa
Daniel <60565717+Galiafer@users.noreply.github.com>
2022-06-12 04:38:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,8 @@ def getTranslation():
     return json.load(translationFile)
 
 
-# get config
 config = getConfig()
-# get translations
 translation = getTranslation()
-# if Windows True, else False (mac, linux)
-isWindows = True if os.name == 'nt' else False
-# if language = 'ru' set translation to Russian, else English
 language = 'ru' if locale.getdefaultlocale()[0] == "ru_RU" else 'en'
 
 if "magiceden.io" in config['launchpadLink']:
